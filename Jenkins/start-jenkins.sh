4188ded8c49dad611c89b408dbbaef232e38c0c4
vermeil2 <126759739+vermeil2@users.noreply.github.com>
2026-02-10 23:21:19 +0900

    start.sh 분리

--- Jenkins/start-jenkins.sh
+++ Jenkins/start-jenkins.sh
@@ -1,3 +1,11 @@
+#!/bin/bash
+set -e
+
+# ==========================================
+# Jenkins 서비스 시작
+# ==========================================
+
+echo "[Step 1] Jenkins 서비스 시작..."
 sudo systemctl enable jenkins
 
 sudo systemctl start jenkins
